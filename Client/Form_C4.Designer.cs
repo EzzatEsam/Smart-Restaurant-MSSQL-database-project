5f609255b42533778ff081dc31512b60bdbd679e
AmrElmasry14 <s-amr.elmasry@zewailcity.edu.eg>
2021-05-21 23:01:02 +0200

    bdayt5

--- Client/Form_C4.Designer.cs
+++ Client/Form_C4.Designer.cs
@@ -41,9 +41,9 @@ namespace Client
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(29, 52);
+            this.label1.Location = new System.Drawing.Point(25, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 46);
+            this.label1.Size = new System.Drawing.Size(80, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Hello";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -53,9 +53,10 @@ namespace Client
             this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button1.Location = new System.Drawing.Point(250, 110);
+            this.button1.Location = new System.Drawing.Point(219, 82);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(307, 48);
+            this.button1.Size = new System.Drawing.Size(269, 36);
             this.button1.TabIndex = 3;
             this.button1.Text = "Make another order";
             this.button1.UseVisualStyleBackColor = false;
@@ -66,9 +67,10 @@ namespace Client
             this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button2.Location = new System.Drawing.Point(250, 250);
+            this.button2.Location = new System.Drawing.Point(219, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(307, 47);
+            this.button2.Size = new System.Drawing.Size(269, 35);
             this.button2.TabIndex = 4;
             this.button2.Text = "Ask for contact";
             this.button2.UseVisualStyleBackColor = false;
@@ -79,9 +81,10 @@ namespace Client
             this.button3.BackColor = System.Drawing.SystemColors.GrayText;
             this.button3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button3.Location = new System.Drawing.Point(250, 317);
+            this.button3.Location = new System.Drawing.Point(219, 238);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(307, 46);
+            this.button3.Size = new System.Drawing.Size(269, 34);
             this.button3.TabIndex = 5;
             this.button3.Text = "View orders List";
             this.button3.UseVisualStyleBackColor = false;
@@ -91,9 +94,10 @@ namespace Client
             this.button4.BackColor = System.Drawing.SystemColors.GrayText;
             this.button4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button4.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button4.Location = new System.Drawing.Point(250, 381);
+            this.button4.Location = new System.Drawing.Point(219, 286);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(307, 46);
+            this.button4.Size = new System.Drawing.Size(269, 34);
             this.button4.TabIndex = 6;
             this.button4.Text = "Checkout";
             this.button4.UseVisualStyleBackColor = false;
@@ -103,25 +107,27 @@ namespace Client
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(29, 188);
+            this.label3.Location = new System.Drawing.Point(25, 141);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(434, 46);
+            this.label3.Size = new System.Drawing.Size(341, 37);
             this.label3.TabIndex = 7;
             this.label3.Text = "Need help ? Ask for contact";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form_C4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form_C4";
             this.Text = "Form_C4";
             this.Load += new System.EventHandler(this.Form_C4_Load);
--- Client/Form_C5.cs
+++ Client/Form_C5.cs
@@ -14,5 +14,34 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form_C6 confirm = new Form_C6();
+            confirm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Form_C2 back = new Form_C2();
+            //start.Show();
+            //this.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_C5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
     }
 }
