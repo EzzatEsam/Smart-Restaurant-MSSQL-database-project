130b6bc5d1e10cc188f22d34e6e43a129a5028d4
Ezzat Esam <s-ezzat.eisawy@zewailcity.edu.eg>
2021-05-21 18:37:25 +0200

    .

--- Db proj/Program.cs
+++ Db proj/Program.cs
@@ -16,7 +16,7 @@ namespace Db_proj
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminStart(null));
+            Application.Run(new F_login());
         }
     }
 }
