e7ad1cf8bb08317df6cd5703d5977795cecee0bf
AmrElmasry14 <s-amr.elmasry@zewailcity.edu.eg>
2021-05-31 21:06:16 +0200

    34anapull

--- Client/DataManager.cs
+++ Client/DataManager.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Client
 {
     public class DataManager
     {
+
     }
 }
--- Client/deffs.cs
+++ Client/deffs.cs
@@ -69,4 +69,5 @@ namespace Client
             cType = tskTYpe;
         }
     }
+
 }
